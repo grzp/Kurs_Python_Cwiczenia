d4f98ff54cd85273e117db2fa165d3acb1501170
Pawlik <pawlikg@play.pl>
2025-05-21 13:21:20 +0200

    testowanie dokładności obliczeń

--- plik2.py
+++ plik2.py
@@ -8,7 +8,7 @@ imie2="Ala"
 nazwisko2="Elementarna"
 stanowisko2="kociara"
 staż2=3.9
-stawka2=0.1*1
+stawka2=0.05 * 2
 
 imie3="Olo"
 nazwisko3="Ratujczak"
@@ -17,9 +17,9 @@ staż3=199
 stawka3=10.1
 
 nagłówek = f"Imię       Nazwisko       Stanowisko   Staż      Stawka"
-napis1 = f"{imie1:10} {nazwisko1:15} {stanowisko1:11} {staż1:^5.0f} {stawka1:>14.10f}"
-napis2 = f"{imie2:10} {nazwisko2:15} {stanowisko2:11} {staż2:^5.0f} {stawka2:>14.10f}"
-napis3 = f"{imie3:10} {nazwisko3:15} {stanowisko3:11} {staż3:^5.0f} {stawka3:>14.10f}"
+napis1 = f"{imie1:10} {nazwisko1:15} {stanowisko1:11} {staż1:^5.0f} {stawka1:>24.20f}"
+napis2 = f"{imie2:10} {nazwisko2:15} {stanowisko2:11} {staż2:^5.0f} {stawka2:>24.20f}"
+napis3 = f"{imie3:10} {nazwisko3:15} {stanowisko3:11} {staż3:^5.0f} {stawka3:>24.20f}"
 
 print(nagłówek)
 print(napis1)
